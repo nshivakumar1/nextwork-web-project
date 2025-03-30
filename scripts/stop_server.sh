ffca19530e4dff4d46b2d3e82821a46a85fbe9c1
Nakul Shivakumar <nakul.shivakumar@nextwork.org>
2025-03-30 14:16:57 +0000

    Added typo

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 isExistApp="$(pgrep httpd)"
 if [[ -n $isExistApp ]]; then
-sudo systemctl stop httpd.service
+sudo systemctll stop httpd.service
 fi
 isExistApp="$(pgrep tomcat)"
 if [[ -n $isExistApp ]]; then
